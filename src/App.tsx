7710be5794afc32221b43e7a850d0a02ee646701
josemiguel736 <robertz@youremail.com>
2025-02-06 12:48:51 +0100

    temporal login

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import {Routes, Route,Outlet, Navigate} from "react-router-dom"
 import Layout from './components/layout/Layout'
 import AdvertsPage from './pages/adverts/AdvertsPage'
+import LoginPage from "./pages/auth/LoginPage"
 
 function App() {
 
@@ -14,7 +15,7 @@ function App() {
       <Route path="new" element={<AdvertsPage/>}/>
 
       </Route>
-
+      <Route path="/login" element={<LoginPage/>}/>
       <Route path="/" element={<Navigate to="/adverts"/>}/>
     </Routes>
   )
--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,8 +1,66 @@
+import { useState } from "react";
+import FormField from "../../components/shared/FormField";
+import Button from "../../components/shared/Button";
+import { login } from "./service";
+import { useLocation, useNavigate } from "react-router-dom";
 
 
 
 function LoginPage(){
+    const location = useLocation()
+    const navigate = useNavigate()
+
+    const [userEmail,setUserEmail] = useState("")
+    const handleUserEmail = (event:React.ChangeEvent<HTMLInputElement>) =>{
+        setUserEmail(event.target.value) }
+
+    const [userPassword,setUserPassword] = useState("")
+    const handleUserPassword = (event:React.ChangeEvent<HTMLInputElement>) =>{
+        setUserPassword(event.target.value)
+    }
+
+    const handleSubmit = async (event:React.FormEvent<HTMLFormElement>) =>{
+        event.preventDefault()
+        try {
+            await login({
+                email:userEmail,
+                password:userPassword
+            })
+            const to = location.state?.from ?? "/"
+            navigate(to,{replace:true})
+
+        } catch (error) {
+            console.log(error)
+        /* TODOOOOOOOOO*/
+            
+        }
+    }
+
     return <div className="container">
         <h1>Login</h1>
+        <form onSubmit={handleSubmit} >
+            <FormField
+            type="text"
+            name="email"
+            label="Email"
+            value={userEmail}
+            onChange={handleUserEmail}
+            />
+            <FormField
+            type="password"
+            name="password"
+            label="Contrsaseña"
+            value={userPassword}
+            onChange={handleUserPassword}
+            />
+        
+        <Button
+        $variant="primary"
+        type="submit">
+            Iniciar Sesión
+        </Button>
+        </form>
     </div>
   }
+
+  export default LoginPage
