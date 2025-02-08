c928e07f0f5a00ea98b88076050089796d30afb4
josemiguel736 <robertz@youremail.com>
2025-02-08 13:21:51 +0100

    Header states

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,17 +1,23 @@
 import { Link, NavLink } from "react-router-dom";
 import ReactLogo from "../../assets/reactLogo";
 import AuthButton from "../shared/AuthButton";
+import { useAuth } from "../../pages/auth/context";
 
+export default function Header() {
+  const { isLogged } = useAuth();
 
-export default function Header(){
-    return (
-        <header className=" bg-sky-900 h-1/10 max-h-[60px]  flex items-center px-4">
-           <Link to="/adverts"><ReactLogo/></Link> 
-            <nav className="text-amber-50 flex w-screen justify-end gap-20 items-center" >
-                <NavLink to="/adverts/new">Nuevo producto</NavLink>
-                <NavLink to="/adverts">Productos</NavLink>
-            <AuthButton/>
-            </nav>
-        </header>
-    )
+  return (
+    <header className=" bg-sky-900 h-1/10 max-h-[60px]  flex items-center px-4">
+      <Link to="/adverts">
+        <ReactLogo />
+      </Link>
+      {isLogged ? (
+        <nav className="text-amber-50 flex w-screen justify-end gap-20 items-center">
+          <NavLink to="/adverts/new">Nuevo producto</NavLink>
+          <NavLink to="/adverts">Productos</NavLink>
+          <AuthButton />
+        </nav>
+      ) : null}
+    </header>
+  );
 }
