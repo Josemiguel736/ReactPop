6d254a42dba94987c21becdaeb5eeadfdab3b25c
josemiguel736 <robertz@youremail.com>
2025-02-08 15:02:12 +0100

    Confirm Logout

--- src/components/shared/AuthButton.tsx
+++ src/components/shared/AuthButton.tsx
@@ -1,21 +1,56 @@
-import { Link } from "react-router-dom";
 import { useAuth } from "../../pages/auth/context";
 import { logout } from "../../pages/auth/service";
 import Button from "./Button";
+import { ComponentProps, useState } from "react";
 
+export interface ConfirmLogoutProps extends ComponentProps<"input"> {
+    setIsClicked:React.Dispatch<React.SetStateAction<boolean>>
+}
 
+ function ConfirmLogout({setIsClicked}:ConfirmLogoutProps) {
+  const [confirm, setConfirm] = useState(false);
+  const { onLogout } = useAuth();
 
-export default function AuthButton(){
-    const {isLogged,onLogout} = useAuth()
-
-    const handleLogoutClick = async () =>{
-        logout()
-        onLogout()
+  const handleSubmit = () => {
+    if (confirm) {
+      logout();
+      onLogout();
     }
+  };
 
-    return isLogged ? (
-        <Button onClick={handleLogoutClick} $variant="secondary">Logout</Button>
-    ) : (
-        <Button $variant="primary" as={Link} to="/login">Login</Button>
-    )
+
+  return (
+    <form onSubmit={handleSubmit} className=" bg-sky-700 text-amber-50">
+      <Button
+        onClick={() => {
+          setIsClicked(false);
+        }}
+        $variant="primary"
+      >
+        Mantenme iniciado
+      </Button>
+      <Button
+        type="submit"
+        onClick={() => {
+          setConfirm(true);
+        }}
+        $variant="secondary"
+      >
+        Cerrar sesión
+      </Button>
+    </form>
+  );
+}
+
+export default function AuthButton() {
+  const [isClicked, setIsClicked] = useState(false)
+  
+
+  return isClicked ? (
+    <ConfirmLogout setIsClicked={setIsClicked} ></ConfirmLogout>
+  ) : <div>
+  <Button onClick={()=>setIsClicked(true)} $variant="secondary">
+    Logout
+  </Button></div>
+  
 }
