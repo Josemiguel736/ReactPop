6e5741b28accfaa3de343edfc88ba2f77f490f54
josemiguel736 <robertz@youremail.com>
2025-03-03 14:05:21 +0100

    link EmpyList, EmptyFilterList

--- src/pages/adverts/EmpltyList.tsx
+++ src/pages/adverts/EmpltyList.tsx
@@ -1,20 +1,19 @@
-import { useNavigate } from 'react-router-dom';
 import Button from '../../components/shared/Button';
 
 const EmptyList = () => {
-	const navigate = useNavigate();
 	return (
 		<div className="bg-sky-600 h-full flex  justify-center text-amber-50">
 			<div className="flex flex-col justify-center gap-3 content-center items-center ">
 				<h1 className="text-3xl">Vaya parece que no hay anuncios!</h1>
 				<h2 className="text-2xl">Quieres ser el primero?</h2>
-				<Button
-					onClick={() => navigate('/adverts/new')}
+				
+				<a href='/adverts/new'>
+				<Button 					
 					className="mb-2"
 					$variant="primary"
 				>
 					Publica un anuncio
-				</Button>
+				</Button> </a>
 			</div>
 		</div>
 	);
